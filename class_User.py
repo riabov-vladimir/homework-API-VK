b7ed3a4a7d88826776591cf3b9672959874ccad0
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-06-08 16:01:03 +0300

    На проверку

--- class_User.py
+++ class_User.py
@@ -1,16 +1,13 @@
-from pprint import pprint
-from urllib.parse import urlencode
-import requests
 from friends_get import friends_get
 
 access_token = 'f10874588a9af88d782aeacdc3cca2fb85a3e72b23200f3bb07d5f0d3abdaf8d67a15a19ccbfbfb40ef64'
 
 
 class User:
+
 	def __init__(self, user_id: int, access_token: str) -> None:
 		self.token = access_token
 		self.user_id = user_id
-		self.friends = friends_get(user_id)
 
 	def	get_params(self):
 		return {
@@ -19,9 +16,14 @@ class User:
 			'user_id': self.user_id
 		}
 
-	def friends_in_common(self, other) -> list:
-		result = list(set(self.friends).intersection(other.friends))
+	def friends_list(self) -> list:
+		result = friends_get(self.user_id)
 		return result
+
+	def __and__(self, other_user):
+		mutual_friends_ids = list(set(self.friends_list()).intersection(other_user.friends_list()))
+		result = map(lambda x: User(x, access_token), mutual_friends_ids)
+		return list(result)
 
 	def __str__(self):
 		return 'https://vk.com/id{}'.format(self.user_id)
--- friends_get.py
+++ friends_get.py
@@ -1,14 +1,11 @@
-# https://vk.com/dev/friends.get
-# https://vk.com/dev/friends.getOnline
 import requests
-import json
-from pprint import pprint
 
+from pprint import pprint
 
 access_token = '1f537a7712ebb37dec7e05b35cd46de6203b36a44e658da7e0f5d4a7c946b65531da896b6b8cc9df1ec4d' # scope_friends
 
 
-def friends_get(user_id: int = 145656) -> list:
+def friends_get(user_id: int) -> list:
 	request_url = 'https://api.vk.com/method/friends.get'
 	params = {
 		'access_token': access_token,
@@ -21,5 +18,8 @@ def friends_get(user_id: int = 145656) -> list:
 	response = requests.get(request_url, params=params)
 	json_ = response.json()['response']['items']
 	return json_
-#
-# pprint(friends_get())
+
+
+if __name__ == "__main__":
+
+	pprint(friends_get(355070))
